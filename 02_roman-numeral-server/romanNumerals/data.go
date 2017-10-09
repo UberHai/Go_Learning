adf75483cea13295c934dbc866069686880f8dc8
UberHai <32371762+UberHai@users.noreply.github.com>
2017-10-09 11:36:53 -0500

    Added roman numerals to 20 & .gitignore

--- 02_roman-numeral-server/romanNumerals/data.go
+++ 02_roman-numeral-server/romanNumerals/data.go
@@ -1,6 +1,16 @@
 package romanNumerals
 
 var Numerals = map[int]string{
+	20: "XX",
+	19: "XIX",
+	18: "XVIII",
+	17: "XVII",
+	16: "XVI",
+	15: "XV",
+	14: "XIV",
+	13: "XIII",
+	12: "XII",
+	11: "XI",
 	10: "X",
 	9: "IX",
 	8: "VIII",
